694505a5532c40eec4ee0c62139a5916379a3536
dennis2311 <58165780+dennis2311@users.noreply.github.com>
2022-02-22 23:04:43 +0900

    좌표 선택 분리 + 합성 케이스 분류

--- src/Page/Synthesis/SynthesisProvider.tsx
+++ src/Page/Synthesis/SynthesisProvider.tsx
@@ -3,22 +3,36 @@ import axios from "axios";
 import { ChildrenProp } from "../../Constant";
 
 interface SynthesisContextProp {
+    authorizedPhotoURL: string | null;
     photoURL: string | null;
     videoURL: string | null;
+    authorizedImageCoord: number[];
     imageCoord: number[];
+    synthesizedPhotoURL: string | null;
     synthesizedVideoURL: string | null;
     handleFileUpload: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    getImageCoord: (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => void;
+    setAuthorizedImageCoord: (value: number[]) => void;
+    setImageCoord: (value: number[]) => void;
+    getImageCoord: (
+        e: React.MouseEvent<HTMLImageElement, MouseEvent>
+    ) => number[];
     requestSynthesize: () => void;
 }
 
 const InitialSynthesisContext: SynthesisContextProp = {
+    authorizedPhotoURL: null,
     photoURL: null,
     videoURL: null,
+    authorizedImageCoord: [0, 0],
     imageCoord: [0, 0],
+    synthesizedPhotoURL: null,
     synthesizedVideoURL: null,
     handleFileUpload: () => {},
-    getImageCoord: () => {},
+    setAuthorizedImageCoord: () => {},
+    setImageCoord: () => {},
+    getImageCoord: () => {
+        return [];
+    },
     requestSynthesize: () => {},
 };
 
@@ -26,11 +40,21 @@ const SynthesisContext = createContext(InitialSynthesisContext);
 export const useSynthesisContext = () => useContext(SynthesisContext);
 
 export default function SynthesisProvider({ children }: ChildrenProp) {
+    const [authorizedPhoto, setAuthorizedPhoto] = useState<null | File>(null);
     const [photo, setPhoto] = useState<null | File>(null);
     const [video, setVideo] = useState<null | File>(null);
+    const [authorizedPhotoURL, setAuthorizedPhotoURL] = useState<string | null>(
+        null
+    );
     const [photoURL, setPhotoURL] = useState<string | null>(null);
     const [videoURL, setVideoURL] = useState<string | null>(null);
+    const [authorizedImageCoord, setAuthorizedImageCoord] = useState<number[]>([
+        0, 0,
+    ]);
     const [imageCoord, setImageCoord] = useState<number[]>([0, 0]);
+    const [synthesizedPhotoURL, setSynthesizedPhotoURL] = useState<
+        string | null
+    >(null);
     const [synthesizedVideoURL, setSynthesizedVideoURL] = useState<
         string | null
     >(null);
@@ -41,18 +65,39 @@ export default function SynthesisProvider({ children }: ChildrenProp) {
 
         const inputFile: File = e.target.files[0];
         if (imageType.includes(inputFile.type)) {
-            if (inputFile.name != "authorized-vitamin-image.png") {
-                alert(
-                    "등록되지 않은 이미지입니다. 관리자에게 제품의 동영상을 촬영하여 보내주시면(ship9136@naver.com), 빠른 시일 내에 제품을 등록하겠습니다."
-                );
-                return;
+            // 사진 파일인 경우
+            if (isAuthorizedImage(inputFile.name)) {
+                // 등록된 사진인 경우
+                setAuthorizedPhoto(inputFile);
+                setAuthorizedPhotoURL(URL.createObjectURL(inputFile));
+            } else {
+                // 일반 사진인 경우
+                if (video) {
+                    //// 이미 동영상을 먼저 선택한 경우
+                    alertUnacceptableImage();
+                } else {
+                    setPhoto(inputFile);
+                    setPhotoURL(URL.createObjectURL(inputFile));
+                }
             }
-            setPhoto(inputFile);
-            setPhotoURL(URL.createObjectURL(inputFile));
         } else {
+            // 동영상 파일인 경우
+            setPhoto(null);
+            setPhotoURL(null);
             setVideo(inputFile);
             setVideoURL(URL.createObjectURL(inputFile));
         }
+    }
+
+    function isAuthorizedImage(imageName: string) {
+        if (imageName == "authorized-vitamin-image.png") return true;
+        return false;
+    }
+
+    function alertUnacceptableImage() {
+        alert(
+            "등록되지 않은 이미지입니다. 관리자에게 제품의 동영상을 촬영하여 보내주시면(ship9136@naver.com), 빠른 시일 내에 제품을 등록하겠습니다."
+        );
     }
 
     function getImageCoord(e: React.MouseEvent<HTMLImageElement, MouseEvent>) {
@@ -63,7 +108,7 @@ export default function SynthesisProvider({ children }: ChildrenProp) {
         const imageHeigth = e.currentTarget.offsetHeight;
         const xcoord = Math.round((xDistance / imageWidth) * 10000) / 100;
         const ycoord = Math.round((yDistance / imageHeigth) * 10000) / 100;
-        setImageCoord([xcoord, ycoord]);
+        return [xcoord, ycoord];
     }
 
     async function requestSynthesize() {
@@ -89,27 +134,19 @@ export default function SynthesisProvider({ children }: ChildrenProp) {
             .catch((error) => {
                 console.log(error);
             });
-
-        // await axios
-        //     .post<string>(`http://localhost:5000/synthesis`, formData, {
-        //         headers: {
-        //             "Content-Type": "multipart/form-data",
-        //         },
-        //     })
-        //     .then((res) => {
-        //         // setSynthesizedVideo(res.data);
-        //     })
-        //     .catch((error) => {
-        //         console.log(error);
-        //     });
     }
 
     const synthesisContext = {
+        authorizedPhotoURL,
         photoURL,
         videoURL,
+        authorizedImageCoord,
         imageCoord,
+        synthesizedPhotoURL,
         synthesizedVideoURL,
         handleFileUpload,
+        setAuthorizedImageCoord,
+        setImageCoord,
         getImageCoord,
         requestSynthesize,
     };
